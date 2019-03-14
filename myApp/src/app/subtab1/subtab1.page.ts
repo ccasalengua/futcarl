f763a82ae31916a398452da32edd640c9ae19ecf
Carlos Casalengua <ccasalengua@gmail.com>
2019-03-14 18:45:43 +0100

    Structure config

--- myApp/src/app/subtab1/subtab1.page.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-subtab1',
-  templateUrl: 'subtab1.page.html',
-  styleUrls: ['subtab1.page.scss']
-})
-export class SubTab1Page {}
--- myApp/src/app/subtabs/subtabs.page.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-subtabs',
-  templateUrl: 'subtabs.page.html',
-  styleUrls: ['subtabs.page.scss']
-})
-export class SubTabsPage {}
